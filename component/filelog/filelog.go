faf835d411cf53ec74c274f669815dc9ab65bceb
aishwaryaa2000 <aishwaryaramani.anand@gmail.com>
2022-02-13 00:10:05 +0530

    Adding file log

--- component/filelog/filelog.go
+++ component/filelog/filelog.go
@@ -1,15 +1,17 @@
 package filelog
 import(
 	"os"
+	"time"
 )
 func WriteUserLog(username,action string){
 	f, err := os.OpenFile("userlog.txt", os.O_APPEND|os.O_WRONLY, 0644) 
 	if err != nil {
 		panic(err)
 	}
-	
+	currentTime := time.Now()
+ 
 	defer f.Close()
-	text := username + action + "\n"
+	text := username + action + " at " +currentTime.Format("2006-01-02 3:4:5 PM") + "\n"
 	_, errr := f.WriteString(text) 
 	if errr != nil {
 		panic(err)
